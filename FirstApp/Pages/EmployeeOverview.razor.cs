34c7d3a4d49b50f19cd4928ee325caa0335d657f
Atef Ataya <atef.ataya@gmail.com>
2023-10-06 18:34:29 +0400

    Commit All

---
+++ FirstApp/Pages/EmployeeOverview.razor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using BethanysPieShopHRM.Shared.Domain;
+
+namespace FirstApp.Pages
+{
+	public partial class EmployeeOverview
+	{
+		public List<Employee>? Employees { get; set; } = default!;
+
+        protected override void OnInitialized()
+        {
+            Employees = Models.MockDataService.Employees;
+        }
+    }
+}
+
